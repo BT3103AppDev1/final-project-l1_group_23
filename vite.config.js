c82913376e36d262de4d4180d544467055ff5f5a
kphoon <e0957785@u.nus.edu>
2026-03-25 15:46:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,3 +16,16 @@ export default defineConfig({
     },
   },
 })
+
+export default defineConfig({
+  base: '/final-project-l1_group_23/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
