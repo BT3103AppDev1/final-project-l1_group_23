ab7c88694351aa3fc344a49ef2fd40e191dd6ee0
YilongMa2 <elong1703@gmail.com>
2026-04-19 23:09:56 +0800

    Live deployment

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/final-project-l1_group_23/',
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
